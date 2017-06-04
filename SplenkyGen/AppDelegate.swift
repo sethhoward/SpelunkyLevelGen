981647e3cf32176b5f6c3a5f5217d5470a3a4f03
sethhoward <s3th.howard@gmail.com>
2017-06-04 14:27:49 -0600

    Initial Commit

---
+++ SplenkyGen/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SplenkyGen
+//
+//  Created by Seth Howard on 6/4/17.
+//  Copyright © 2017 Seth Howard. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
