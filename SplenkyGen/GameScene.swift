a51d256ef0f72be2fb646104f60b7cdad7ae05e7
sethhoward <s3th.howard@gmail.com>
2017-06-05 20:10:31 -0600

    initial layout

--- SplenkyGen/GameScene.swift
+++ SplenkyGen/GameScene.swift
@@ -9,79 +9,177 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+extension CGSize {
+    public init(rows: Int, cols: Int) {
+        self = CGSize(width: rows, height: cols)
+    }
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var rows: Int {
+        return Int(self.width)
+    }
+    
+    var cols: Int {
+        return Int(self.height)
+    }
+}
+
+class RoomCell {
+    let background: SKSpriteNode
+    
+    var position: CGPoint {
+        get {
+            return background.position
+        }
+        set(newPosition) {
+            background.position = newPosition
+        }
+    }
+    
+    var bgColor: SKColor {
+        set(newColor) {
+            background.color = newColor
+        }
+        get {
+            return background.color
+        }
+    }
+    
+    init(position: CGPoint, size: CGSize) {
+        background = SKSpriteNode(color: .clear, size: size)
+        background.colorBlendFactor = 1
+        // The positions should be static
+        self.position = position
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+    }
+}
+
+var rgb: (CGFloat, CGFloat, CGFloat) = (0.0, 0.0, 0.0)
+var count = -1
+func randomRPG() -> SKColor {
+    if rgb.0 >= 254 {
+        rgb.0 = 0
+    }
+    if rgb.1 >= 254 {
+        rgb.1 = 0
+    }
+    if rgb.2 >= 254 {
+        rgb.2 = 0
+    }
+    
+    switch count {
+    case -1:
+        rgb.0 += 3.1875
+    case 0:
+        rgb.0 += 3.1875
+    case 1:
+        rgb.1 += 3.1875
+    case 2:
+        rgb.2 += 3.1875
+    default:
+        rgb = (0, 0, 0)
+        count = 0
+        return randomRPG()
+    }
+    
+    return SKColor(calibratedRed: rgb.0/255, green: rgb.1/255, blue: rgb.2/255, alpha: 1)
+}
+
+// 10 x 8 collection of sprites that make up a room
+class Room: SKNode {
+    private let screenSize = CGSize(rows: 8, cols: 10)
+    private let spriteSize = CGSize(width: 32, height: 32)
+    
+    private lazy var roomLayoutNodes: [[RoomCell]] = {
+        rgb.0 = 0
+        rgb.1 = 0
+        rgb.2 = 0
+        var nodes = [[RoomCell]]()
+        for cols in 0...self.screenSize.cols {
+            var row = [RoomCell]()
+            for rows in 0...self.screenSize.rows {
+                let position = CGPoint(x: CGFloat(cols) * self.spriteSize.width, y: CGFloat(rows) * self.spriteSize.height)
+                let roomCell = RoomCell(position: position, size: self.spriteSize)
+                roomCell.bgColor = randomRPG()
+                row.append(roomCell)
+            }
+            nodes.append(row)
+        }
+        
+        count += 1
+        return nodes
+    }()
+    
+    init(indexX: Int, indexY: Int) {
+        super.init()
+        position = CGPoint(x: (CGFloat(screenSize.cols) * spriteSize.width) * CGFloat(indexX), y: (CGFloat(screenSize.rows) * spriteSize.height) * CGFloat(indexY))
+        for cols in roomLayoutNodes {
+            for node in cols {
+                addChild(node.background)
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// scene consists of 4 x 4 grid of rooms
+class GameScene: SKScene {
+    private let gridSize = CGSize(rows: 4, cols: 4)
+    
+    private lazy var rooms: [[Room]] = {
+        var rooms = [[Room]]()
+        for rows in 0...self.gridSize.rows {
+            var col = [Room]()
+            for cols in 0...self.gridSize.cols {
+                let room = Room(indexX: cols, indexY: rows)
+                col.append(room)
+            }
+            rooms.append(col)
+        }
+        
+        return rooms
+    }()
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //(rooms.flatMap { $0 }).forEach { addChild($0) }
+        for cols in 0...gridSize.cols {
+            for rows in 0...gridSize.rows {
+                addChild(rooms[cols][rows])
+            }
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
+
     }
     
     override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
+
     }
     
     override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
+
     }
     
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+        default:()
         }
     }
     
--- SplenkyGen/ViewController.swift
+++ SplenkyGen/ViewController.swift
@@ -17,15 +17,23 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.frame = CGRect(origin: CGPoint(), size: CGSize(width: 1280, height: 1024))
+        
         if let view = self.skView {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            let scene = GameScene()
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFit
+            scene.size = skView.bounds.size
                 
                 // Present the scene
                 view.presentScene(scene)
-            }
+            
+//            let foo = SKSpriteNode(color: .cyan, size: CGSize(width: 128, height: 128))
+//            foo.anchorPoint = CGPoint()
+//            foo.position = CGPoint(x: 0, y: 0)
+//            scene.addChild(foo)
+                
             
             view.ignoresSiblingOrder = true
             
